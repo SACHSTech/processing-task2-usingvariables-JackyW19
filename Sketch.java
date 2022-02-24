c3586e9a131a8cdd4d2ca98a30a0cfc9660d940a
Jacky Wang <jacky.wang23@ycdsbk12.ca>
2022-02-23 20:56:25 -0500

    Done

--- Sketch.java
+++ Sketch.java
@@ -9,6 +9,8 @@ public class Sketch extends PApplet {
   public void settings() {
 	// put your size call here
     size(400, 400);
+
+    
   }
 
   /** 
@@ -16,21 +18,76 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(51, 204, 255);
+    
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // change width and height here
+    float width = 400;
+    float height = 400;
+
+    // scale factor
+    float scaleFactorW = width / 400;
+    float scaleFactorH = height / 400;
+
+	  // yellow sun
+    fill(255, 255, 0);
+    ellipse(scaleFactorW *0, scaleFactorH *0, scaleFactorW *150, scaleFactorH *150);
+
+    // green rectangle
+    fill(0,255,0);
+    rect(scaleFactorW *0, scaleFactorH *320, scaleFactorW *400, scaleFactorH *430);
+
+    // tan rectangle  
+    fill(210, 180, 140);
+    rect(scaleFactorW *95, scaleFactorH *150, scaleFactorW *210, scaleFactorH *180);
+
+    // light red chimney
+    fill(255, 127, 127);
+    rect(scaleFactorW *250, scaleFactorH *85, scaleFactorW *30, scaleFactorH *50);
+
+    // red chimney crown
+    fill(255, 51, 51);
+    rect(scaleFactorW *245, scaleFactorH *80, scaleFactorW *40, scaleFactorH *10);
+
+    // red triangle roof
+    fill(255, 51, 51);
+    triangle(scaleFactorW *80, scaleFactorH *150, scaleFactorW *200, scaleFactorH *70, scaleFactorW *320, scaleFactorH *150);
+
+    // left rectangle window
+    fill(51, 153, 255);
+    rect(scaleFactorW *115, scaleFactorH *180, scaleFactorW *55, scaleFactorH *55);
+
+    // vertical left window line
+    line(scaleFactorW *143, scaleFactorH *180, scaleFactorW *143, scaleFactorH *235);
+
+    // horizontal left window line
+    line(scaleFactorW *115, scaleFactorH *206, scaleFactorW *170, scaleFactorH *206);
+
+    // right rectangle window
+    fill(51, 153, 255);
+    rect(scaleFactorW *230, scaleFactorH *180, scaleFactorW *55, scaleFactorH *55);
+
+    // vertical right window line
+    line(scaleFactorW *259, scaleFactorH *180, scaleFactorW *259, scaleFactorH *235);
+
+    // horizontal right window line
+    line(scaleFactorW *230, scaleFactorH *205, scaleFactorW *285, scaleFactorH *205);
+
+    // orange door 
+    fill(255, 204, 0);
+    rect(scaleFactorW *180, scaleFactorH *260, scaleFactorW *45, scaleFactorH *70);
+
+    // brown doorknob
+    fill(101, 67, 33);
+    ellipse(scaleFactorW *190, scaleFactorH *295, scaleFactorW *7, scaleFactorH *7);
+
+    // grey pathway
+    fill(211, 211, 211);
+    rect(scaleFactorW *180, scaleFactorH *330, scaleFactorW *45, scaleFactorH *200);
+
   }
   
-  // define other methods down here.
 }
